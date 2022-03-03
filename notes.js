7b2e8ca15f8a363d1118c51331338c9589b54aed
Mannika <mannika2001verma@gmail.com>
2022-03-03 23:42:28 +0530

    Initial commit

--- notes.js
+++ notes.js
@@ -4,6 +4,7 @@ showNotes();
 let addbtn = document.getElementById('addbtn');
 addbtn.addEventListener("click", function (e) {
     let addtxt = document.getElementById("addtext");
+    let addTitle = document.getElementById("addTitle");
     let notes = localStorage.getItem("notes");
     if (notes == null) {
         notesObj = [];
@@ -11,7 +12,11 @@ addbtn.addEventListener("click", function (e) {
     else {
         notesObj = JSON.parse(notes);
     }
-    notesObj.push(addtxt.value);
+    let myObj = {
+        title: addTitle.value, 
+        text: addtxt.value
+    }
+    notesObj.push(myObj);
     localStorage.setItem("notes", JSON.stringify(notesObj));
     addtxt.value = "";
     console.log(notesObj);
@@ -31,8 +36,8 @@ function showNotes() {
         html += `
     <div class=" my-2 mx-2 card" style="width: 18rem;">
     <div class="card-body">
-      <h5 class="card-title">Note ${index + 1}</h5>
-      <p class="card-text"> ${element}</p>
+      <h5 class="card-title"> ${element.title}</h5>
+      <p class="card-text"> ${element.text}</p>
       <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Node</button>
     </div>
 </div>
@@ -61,18 +66,19 @@ function deleteNote(index){
     localStorage.setItem("notes", JSON.stringify(notesObj));
     showNotes();
 }
-let search = document.getElementById("searchtxt");
+let search = document.getElementById('searchtxt');
 search.addEventListener("input",function(){
     let inputVal = search.value;
     console.log("input event fired!", inputVal);
-    let notecards = document.getElementsByClassName("noteCards");
-    Array.from(noteCards).forEach(function(element){
+    let notecards = document.getElementsByClassName("card");
+    Array.from(notecards).forEach(function(element){
         let cardtxt = element.getElementsByTagName("p")[0].innerText;
-        if(cardtxt.include(inputVal)){
+        if(cardtxt.includes(inputVal)){
             element.style.display = "block";
         }
         else{
             element.style.display = "none";
         }
+
     })
 })
